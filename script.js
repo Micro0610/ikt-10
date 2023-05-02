5a0a528a4c32e08d995f19d32bf3754ec68d004e
Micro0610 <blizzardbon@gmail.com>
2023-05-02 19:33:13 +0200

    Create script.js Update main.html, maincss.css
    Megcsináltam a JS fájlt, majd csináltam egy szöveg dobozt minden kép alá. Minden kép gombként is funkcionál!

---
+++ script.js
@@ -0,0 +1,15 @@
+function hbolink(){
+    location.href="https://www.google.com"
+}
+
+function netflixlink(){
+    location.href="https://www.google.com"
+}
+
+function disneylink(){
+    location.href="https://www.google.com"
+}
+
+function skyshowtimelink(){
+    location.href="https://www.google.com"
+}
